06de370f615ba0021bcd4e0e4f440dab96c1f6e5
Ivan <ivan@localhost.localdomain>
2016-09-10 12:06:37 -0500

    editando los modelos

--- Administrador/models.py
+++ Administrador/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Perfil(models.Model):
+	GENEROS=(
+		('Hombre','Hombre'),
+		('Mujer','Mujer')
+		)
+	TIPO=(
+		('Maestro', 'Maestro'),
+		('Alumno', 'Alumno')
+		)
+	fecha_nacimiento=models.DataFiel(null=True,blank=True)
+	Nivel=(
+		('Maestro','Maestro'),
+		('Alumno','Alumno')
+		)
+	sexo=models.CharField(max_length=140,Choices=GENEROS,default="Hombre")
+	bio = models.TexField(null=True,blank=True)
+	is_user=models.CharField(max_length=140,Choices=TIPO,default="Alumno")
+# Create your models h
