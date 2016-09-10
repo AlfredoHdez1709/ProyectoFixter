9aea70dc4dd4aa4239febebf28b83e74d516072b
Ivan <ivan@localhost.localdomain>
2016-09-10 15:44:01 -0500

    se modificaron los archivos para subir imagenes y verlas

--- sitio/urls.py
+++ sitio/urls.py
@@ -2,9 +2,16 @@ from django.conf.urls import url,include
 from django.contrib import admin
 from Administrador import urls as AdministradorUrls
 from Principal import urls as PrincipalUrls
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^',include(AdministradorUrls)),
     url(r'^',include(PrincipalUrls)),
+    url(
+    	regex=r'^media/(?P<path>.*)$',
+    	view=serve,
+    	kwargs={"document_root":settings.MEDIA_ROOT}
+    	)
 ]
